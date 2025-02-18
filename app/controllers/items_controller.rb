1dcab347f7543afa48069364a3032d5af932cc09
yelmasry <yarra.elmasry@gmail.com>
2025-02-18 15:06:02 +0000

    final

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,4 +4,19 @@ class ItemsController < ApplicationController
 
     render({ :template => "item_templates/list" })
   end
+
+  def display_form 
+    render({ :template => "item_templates/backdoor_form"})
+  end 
+
+  def create_item 
+    i = Item.new
+    i.link_url = params.fetch("link_url_param")
+    i.link_description = params.fetch("link_description_param")
+    i.thumbnail_url = params.fetch("thumbnail_url_param")
+    i.save 
+    
+    #render({ :template => "item_templates/create"})
+    redirect_to("/")
+  end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
 
   get("/", { :controller => "items", :action => "index" })
+
+  get("/backdoor", { :controller => "items", :action => "display_form"}) 
+
+  post("/make_a_new_item", { :controller => "items", :action => "create_item"})
+
 end
